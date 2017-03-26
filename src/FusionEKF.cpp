6494f8951a27728de729b9fcbd059dbe051d661d
Fisher Shi <fisher.jianyu.shi@gmail.com>
2017-03-26 00:08:12 -0400

    update Q matrix based on dt and pre-set ax&ay

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -34,18 +34,14 @@ FusionEKF::FusionEKF() {
     Hj_ = MatrixXd(3, 4);
 
     R_laser_ = MatrixXd(2, 2);
-    R_laser_ << 1, 0,
-                0, 1;
+    R_laser_ << 0.0225, 0,
+                0,      0.0225;
     R_radar_ = MatrixXd(3, 3);
-    R_radar_ << 1, 0, 0,
-                0, 1, 0,
-                0, 0, 1;
+    R_radar_ << 0.09, 0,      0,
+                0,    0.0009, 0,
+                0,    0,      0.09;
 
     ekf_.Q_ = MatrixXd(4, 4);
-    ekf_.Q_ << 1, 1, 1, 1,
-               1, 1, 1, 1,
-               1, 1, 1, 1,
-               1, 1, 1, 1;
 
 }
 
@@ -73,16 +69,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
             double rho = measurement_pack.raw_measurements_[0];
             double phi = measurement_pack.raw_measurements_[1];
-            ekf_.x_ << cos(phi)*rho, sin(phi)*rho, 1, 1;
-            cout << "initial x radar:" << endl;
+            ekf_.x_ << cos(phi)*rho, sin(phi)*rho, 0, 0;
+            cout << "initial x (1st radar measurement):" << endl;
             cout << ekf_.x_ << endl;
 
         }
         else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
             previous_timestamp_ = measurement_pack.timestamp_;
 
-            ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 1, 1;
-            cout << "initial x laser:" << endl;
+            ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+            cout << "initial x (1st laser measurement):" << endl;
             cout << ekf_.x_ << endl;
         }
 
@@ -102,11 +98,30 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
         - Time is measured in seconds.
        * Update the process noise covariance matrix.
      */
+
     double dt = (double)(measurement_pack.timestamp_-previous_timestamp_)/1000000.0;
+    cout << "delta t: " << dt << endl;
+    previous_timestamp_ = measurement_pack.timestamp_;
+
+    // update F matrix based on dt
     ekf_.F_ << 1, 0, dt, 0,
                0, 1, 0,  dt,
                0, 0, 1,  0,
                0, 0, 0,  1;
+
+    // update Q matrix based on dt
+    float dt_2 = dt * dt;
+    float dt_3 = dt_2 * dt;
+    float dt_4 = dt_3 * dt;
+
+    int noise_ax = 5;
+    int noise_ay = 5;
+
+    ekf_.Q_ <<  dt_4/4*noise_ax,    0,               dt_3/2*noise_ax, 0,
+                0,                  dt_4/4*noise_ay, 0,               dt_3/2*noise_ay,
+                dt_3/2*noise_ax,    0,               dt_2*noise_ax,   0,
+                0,                  dt_3/2*noise_ay, 0,               dt_2*noise_ay;
+
     ekf_.Predict();
     cout << "predicted x:" << endl;
     cout << ekf_.x_ << endl;
